7bb01b489730ee7aecc322a96a2583473bdc7954
jerry <519248932@qq.com>
2017-11-08 23:07:21 +0800

    完成首页登录注册功能

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -7,10 +7,13 @@
         </router-link>
         <div class="head-nav">
           <ul class="nav-list">
-              <li @click="logClick">登录</li>
+              <li>{{ username }}</li>
+              <li v-if="username!== ''"class="nav-pile">|</li>
+              <li v-if="username!==''" @click="quit">退出</li>
+              <li v-if="username=== ''" @click="logClick">登录</li>
               <li class="nav-pile">|</li>
-              <li @click="regClick">注册</li>
-              <li class="nav-pile">|</li>
+              <li v-if="username=== ''" @click="regClick">注册</li>
+              <li v-if="username=== ''" class="nav-pile">|</li>
               <li @click="aboutClick">关于</li>
           </ul>
         </div>  
@@ -25,10 +28,10 @@
       <p>© 2016 fishenal MIT</p>
     </div>
      <my-dialog :is-show="isShowLogDialog" @on-close="closeDialog('isShowLogDialog')">
-      <p>登录</p>
+      <log-form @has-log = "onSuccessLog"></log-form>
     </my-dialog>
      <my-dialog :is-show="isShowRegDialog"  @on-close="closeDialog('isShowRegDialog')">
-      <p>注册a</p>
+      <reg-form></reg-form>
     </my-dialog>
     <my-dialog :is-show="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
       <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。 </p>
@@ -38,15 +41,20 @@
 
 <script>
 import MyDialog from './base/dialog'
+import LogForm from './logForm'
+import RegForm from './regForm'
 export default {
   components:{
-    MyDialog
+    MyDialog,
+    LogForm,
+    RegForm 
   },
   data(){
     return {
       isShowAboutDialog: false,
       isShowLogDialog: false,
       isShowRegDialog: false,
+      username:''
     }
   },
   methods:{
@@ -61,6 +69,14 @@ export default {
     },
     closeDialog:function(attr){
       this[attr] = false
+    },
+    onSuccessLog:function (data){
+      console.log(data)
+      this.closeDialog('isShowLogDialog')
+      this.username = data.username 
+    },
+    quit:function(){
+      this.username = ''
     }
   }
 }
