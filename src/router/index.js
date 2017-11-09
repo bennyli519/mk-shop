acb8b7c911c0c3dfb614d8ca4177402c6c48f8ba
jerry <519248932@qq.com>
2017-11-09 23:45:36 +0800

    最后一次优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/index.vue'
+import DetailPage from '../pages/detail.vue'
+import DetailCount from '../pages/detail/count.vue'
+import DetailFore from '../pages/detail/forecast.vue'
+import DetailAna from '../pages/detail/analysis.vue'
+import DetailPublish from '../pages/detail/publish.vue'
 
 Vue.use(Router)
 
@@ -10,6 +15,29 @@ export default new Router({
     {
       path: '/',
       component: IndexPage
+    },{
+      path:'/detail',
+      component: DetailPage,
+      redirect:'/detail/count',
+      children:[
+        {
+          path:'count',
+          component:DetailCount,
+ 
+        },
+        {
+          path:'forecast',
+          component:DetailFore
+        },
+        {
+          path:'analysis',
+          component:DetailAna
+        },
+        {
+          path:'publish',
+          component:DetailPublish
+        }
+      ]
     }
   ]
 })
